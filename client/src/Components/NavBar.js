c6037e426a23645558912949075d700ecdb31f86
Mark <markwilkins86@gmail.com>
2022-07-14 16:22:28 -0400

    Logout Added

--- client/src/Components/NavBar.js
+++ client/src/Components/NavBar.js
@@ -1,8 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
-// import Home from "./Home";
+import { useNavigate } from "react-router-dom"
 
 function NavBar() {
+
+    let navigate = useNavigate();
+
+    function handleLogOut() {
+      fetch("/logout", { method: "DELETE" }).then((r) => {
+        if (r.ok) {
+          setUser(null);
+          navigate("/");
+        }
+      });
+  } 
+  
   return (
     <div>
       <div class="navbar-toggler">
@@ -19,7 +31,7 @@ function NavBar() {
                 </Link>
                 <div >
                   <div class="nav-item">
-                    <Link to="/" class="nav-link" >
+                    <Link to="/" class="nav-link" onClick={handleLogOut}>
                       Log Out
                     </Link>
                   </div>
